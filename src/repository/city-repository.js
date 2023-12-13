acad1ef5100f4a9b982dc561d15055ef3d38274a
yashaswa <sharmayashswa777@gmail.com>
2023-12-14 01:55:08 +0530

    completed cityrepository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,28 +1,57 @@
 const { City } =require('../models/index');
 
 class CityRepository{
-    async  createCity({name}){
-        try{
-            const city = await City.create({name});
-            return city;
-        }
-        catch(error){
-            throw{error};
-        }
+   async createcity({ name }){
+    try{
+        const city = await City.create({
+            name
+        });
+        return city;
     }
+    catch(error){
+        console.log("something went wrong");
+        throw {error};
+    }
+ }
 
-    async deleteCity(cityId){
-        try {
-                await City.destroy({
-                    where: {
-                        id:cityId
-                    }
-                })
-        }
-        catch (error){
-                throw {error};
-        }
+
+   async deletecity(cityId){
+    try{
+        await City.destroy({
+            where :{id:cityId}
+        });
+        return city;
+    } catch(error){
+        console.log("something went wrong");
+        throw {error};
     }
+   }
+
+
+
+   async updatecity(cityId,data){try {
+    const city =await City.update(data,{
+        where :{
+            id:cityId
+        }
+    });
+    return city;
+   } catch (error) {
+    
+   }
+}
+
+
+
+   async getcity(){
+    try {
+        const city =await City.findbyPK(cityId); // instead of where you can use find by pk read in modules queries
+        return City;
+    } catch (error) {
+        console.log("something went wrong");
+        throw {error};
+    }
+   }
 }
 
 module.exports = CityRepository;
